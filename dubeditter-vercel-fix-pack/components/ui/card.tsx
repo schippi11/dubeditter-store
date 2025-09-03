477d998af51cbe5353e308fa27ee82f2f8ed801c
schippi11 <michael.schippers@live.no>
2025-09-03 16:48:56 +0200

    Add files via upload

---
+++ dubeditter-vercel-fix-pack/components/ui/card.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+const cn = (...s: Array<string | undefined>) => s.filter(Boolean).join(" ");
+
+export function Card(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} className={cn("rounded-2xl border bg-white shadow-sm", props.className)} />;
+}
+export function CardHeader(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} className={cn("p-4 border-b", props.className)} />;
+}
+export function CardContent(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} className={cn("p-4", props.className)} />;
+}
+export function CardFooter(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} className={cn("p-4 border-t", props.className)} />;
+}
+export default Card;
